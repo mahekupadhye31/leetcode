19956a8bd06bf994fa154881f4a183c8a6d04474
Mahek Upadhye <114089022+mahekupadhye31@users.noreply.github.com>
2026-09-20 18:43:47 -0700

    Add: course-schedule - submission-2

---
+++ Data Structures & Algorithms/course-schedule/submission-2.py
@@ -0,0 +1,26 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        adj=defaultdict(list)
+        indegree={i:0 for i in range(numCourses)}
+        result=[]
+
+        for u,v in prerequisites:
+            adj[v].append(u)
+            indegree[u]=1+indegree.get(u,0)
+
+        q=deque()
+
+        for i in range(numCourses):
+            if indegree[i]==0:
+                q.append(i)
+        
+        while q:
+            node=q.popleft()
+            result.append(node)
+            for nei in adj[node]:
+                indegree[nei]-=1
+                if indegree[nei]==0: 
+                    q.append(nei)
+        return numCourses==len(result)
+
+
